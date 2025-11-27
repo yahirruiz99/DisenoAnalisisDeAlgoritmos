eecfc9ecc6985f16d36a6cee25103a27aff287fe
Yahir <yahirruiz99@aragon.unam.mx>
2025-11-27 00:07:59 -0600

    Subiendo cambios finales

--- Ejercicio4/MejorCampo.py
+++ Ejercicio4/MejorCampo.py
@@ -45,7 +45,6 @@ for a, b, c in combinations(hull, 3):
         mayor = area
         mejor = (a, b, c)
 
-# Solo agregué estas líneas para mostrar en terminal
 print("Triángulo de mayor área encontrado:")
 for x, y in mejor:
     print(f"{x} {y}")
